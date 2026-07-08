3794456e9644cd40b9eb62a4b6267e1e1220370c
Daniel Molinas <rodanielmolinas@gmail.com>
2026-07-08 18:55:50 -0300

    Actualiza inicializacion de base de datos

--- inicio/crear_base_de_datos.py
+++ inicio/crear_base_de_datos.py
@@ -1,6 +1,6 @@
 import sqlite3
 from pathlib import Path
-from configuracion import RUTA_BD, RUTA_ESQUEMA, verificar_estructura, configuracion_pragmas
+from configuracion import RUTA_DB, RUTA_ESQUEMA, verificar_estructura, configuracion_pragmas
 
 def crear_tablas(conn:sqlite3.Connection,schema_path:Path)->None:
     #"Ejecuta el script de sql del esquema"
@@ -12,16 +12,16 @@ def incializar_base_de_datos()->None:
     #verifica estructura, conecta y crea tablas
     try:
         verificar_estructura()
-        print("✅ Estructura de archivos verificado correctamente...")
+        print("OK: Estructura de archivos verificada correctamente...")
     except FileNotFoundError as e:
-        print(f"❌ Error en la estructura de archivos: {e}")
+        print(f"Error en la estructura de archivos: {e}")
         raise
     
     try:
-        with sqlite3.connect(RUTA_BD)as conn:
+        with sqlite3.connect(RUTA_DB)as conn:
             configuracion_pragmas(conn)
             crear_tablas(conn,RUTA_ESQUEMA)
-            print("✅ Base de datos inicializada correctamente.")
+            print("OK: Base de datos inicializada correctamente.")
     except sqlite3.Error as e:
         print(f"Error de SQLite al crear la base de datos: {e}")
         raise
